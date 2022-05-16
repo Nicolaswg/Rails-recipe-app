2ca2e2179a391b59ee7b522f05a48b3fa873f9b6
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-16 18:13:49 -0400

    Add postgresql setup

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'jbuilder'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
+# postgresql gem
+gem 'pg'
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_16_210001) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
