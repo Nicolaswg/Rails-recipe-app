15b78360ea688ad74dab6764c0a72a7e8ded3b13
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-17 00:18:52 +0300

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'recipe_foods/show'
   get 'foods/index'
   get 'foods/show'
@@ -14,5 +15,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "recipes#index"
 end
