7ecf76c9032c768173875e3f5d679866cf522816
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-16 18:39:59 -0400

    Refactor routes || add name input to sign_up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'recipe_foods/show'
-  get 'foods/index'
-  get 'foods/show'
-  get 'foods/new'
-  get 'foods/create'
-  get 'foods/destroy'
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/new'
-  get 'recipes/create'
-  get 'recipes/destroy'
-  get 'users/index'
+
+	# Defines the root path route ("/")
+	root "users#index"
+	
+	#Resources routes
+	resources :users, only: [:index] do
+		resources :recipes, except: [:edit, :update] 
+		resources :foods, except: [:edit, :update]
+		resources :recipe_foods, only: [:index]
+	end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "recipes#index"
 end
