75769fa45a51a47f2b575122e724611ee23e8518
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-18 00:56:46 +0300

    Add a new recipe with notices and alerts

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,20 +9,32 @@ class RecipesController < ApplicationController
     @recipes = @user.recipes.find(params[:id])
   end
 
-  def new; end
+  def new
+    @user = current_user
+    @recipe = Recipe.new
+  end
 
-  def create; end
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    @recipe.user_id = params[:user_id]
+    respond_to do |format|
+      if @recipe.save!
+        format.html { redirect_to user_recipe_path(@recipe.user_id, @recipe.id), notice: 'Recipe created successfully!' }
+      else
+        format.html { redirect_to new_user_recipes_path, alert: 'Failed to create recipe' }
+      end
+    end
+  end
 
   def destroy
     recipe = Recipe.find(params[:id])
     recipe.destroy!
-    flash[:success] = 'Recipe deleted successfully!'
-    redirect_to user_recipes_path
+    redirect_to user_recipes_path, notice: 'Recipe deleted successfully!'
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description)
+    params.require(:recipes).permit(:name, :description, :preparation_time, :cooking_time, :public)
   end
 end
