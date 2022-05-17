486dac898a4b9ab1de30345dfbb6a00e71dfbffc
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-17 20:44:15 +0300

    Add validation rule to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+  validates :name, presence: true
 end
