69d97224dd198ac2db86d9112a35bf091fbf92b0
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-17 20:45:38 +0300

    Create show view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,11 +3,20 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @recipe = @user.recipes.find(params[:id])
+  end
 
   def new; end
 
   def create; end
 
   def destroy; end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 end
