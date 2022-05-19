5aa9b051d3b4eabd103c535b536fabf4c0d04337
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-19 16:12:08 -0400

    Apply rubocop format

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
-		@recipe = Recipe.where(public: true).order(created_at: :desc).includes(:user, :recipe_foods, recipe_foods: [:food])
-	end
+    @recipe = Recipe.where(public: true).order(created_at: :desc).includes(:user, :recipe_foods,
+                                                                           recipe_foods: [:food])
+  end
 end
